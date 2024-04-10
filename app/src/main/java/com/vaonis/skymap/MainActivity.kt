0cb1bcf1183c04ce4d88d794aa8830fb168939c2
cjanie <janiechun@hotmail.fr>
2024-04-10 21:24:49 +0200

    zoom and pan functionality

--- app/src/main/java/com/vaonis/skymap/MainActivity.kt
+++ app/src/main/java/com/vaonis/skymap/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -19,7 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
@@ -83,14 +87,58 @@ class MainActivity : ComponentActivity() {
                                         columnWidthInPx = coordinates.size.width.toFloat()
                                     }
                             ) {
+                                var scale by remember {
+                                    mutableFloatStateOf(1F)
+                                }
+
+                                var offset by remember {
+                                    mutableStateOf(Offset.Zero)
+                                }
+
                                 val skyViewSizeInPx = if (columnWidthInPx < columnHeightInPx) columnWidthInPx else columnHeightInPx
                                 val skyDiameterInPx = skyRadiusInPx * 2
-                                val scale = skyViewSizeInPx / skyDiameterInPx
+                                val initialScale = skyViewSizeInPx / skyDiameterInPx
+
+
+
                                 // https://www.youtube.com/watch?v=3CjOyoqi_PQ // TODO Zoom
-                                for(astronomicalObject in astronomicalObjects) {
-                                    AstronomicalObjectInSkyComposable(
-                                        astronomicalObject, scale)
+                                BoxWithConstraints(
+
+                                ) {
+
+                                    val state = rememberTransformableState {zoomChange, panChange, rotationChange ->
+                                        scale = (scale * zoomChange).coerceIn(1F, 10F)
+
+                                        val extraWidth = (scale - 1) * constraints.maxWidth
+                                        val extraHeight = (scale - 1) * constraints.maxHeight
+
+                                        val maxX = extraWidth / 2
+                                        val maxY = extraHeight / 2
+                                        offset = Offset(
+                                            x = (offset.x + panChange.x).coerceIn(-maxX, maxX),
+                                            y = (offset.y + panChange.y).coerceIn(-maxY, maxY)
+                                        )
+                                    }
+
+                                    Column(
+                                        modifier = Modifier
+                                            .graphicsLayer {
+                                                scaleX = initialScale
+                                                scaleY = initialScale
+                                                translationX = offset.x
+                                                translationY = offset.y
+
+                                            }
+                                            .transformable(state)
+                                    ) {
+                                        for(astronomicalObject in astronomicalObjects) {
+                                            AstronomicalObjectInSkyComposable(
+                                                astronomicalObject, scale)
+                                        }
+                                    }
+
                                 }
+
                             }
                         }
                     }
